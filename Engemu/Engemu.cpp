b2a4ed5e205c3d4097d29b9fd3d918073ba72dfa
mrRosset <marius.rosset@epfl.ch>
2017-07-09 17:06:41 +0200

    deal with switch to thumb in disassembling

--- Engemu/Engemu.cpp
+++ Engemu/Engemu.cpp
@@ -19,20 +19,35 @@ int main(int argc, char* argv[])
 	cpu.mem.loadRom(argv[3]);
 	E32ImageLoader::load(image, cpu.mem, argv[2]);
 
-	cpu.gprs[Regs::PC] = 0x50392D54;//image.header->code_base_address + image.header->entry_point_offset;
+	cpu.gprs[Regs::PC] = image.header->code_base_address + image.header->entry_point_offset; // 0x50392D54 <- entry of Euser.dll;
 	//TODO: find the correct place where the SP is initialized
 	cpu.gprs[Regs::SP] = 0x7FFF'FFFF; //start of the ram section
 
 	while (true) {
 		for (int i = 0; i < 5; i++) {
-			u32 instr = cpu.mem.read32(cpu.gprs[Regs::PC] + i * 4);
-			IR_ARM ir;
-			try {
-				Decoder::Decode(ir, instr);
-				std::cout << std::hex << cpu.gprs[Regs::PC] + i * 4 << " - " << instr << std::dec << ": " << std::hex << Disassembler::Disassemble(ir) << std::endl;
+			if (cpu.cpsr.flag_T) {
+				u16 instr = cpu.mem.read16(cpu.gprs[Regs::PC] + i * 2);
+				IR_Thumb ir;
+				ir.instr = TInstructions::SWI;
+				try {
+					Decoder::Decode(ir, instr);
+					std::string text = Disassembler::Disassemble(ir);
+					std::cout << std::hex << cpu.gprs[Regs::PC] + i * 2 << " - " << instr << std::dec << ": " << std::hex << Disassembler::Disassemble(ir) << std::endl;
+				}
+				catch (...) {
+					std::cout << std::hex << cpu.gprs[Regs::PC] + i * 2 << " - " << instr << std::dec << ": " << "Unkown instruction" << std::endl;
+				}
 			}
-			catch (...) {
-				std::cout << std::hex << cpu.gprs[Regs::PC] + i * 4 << " - " << instr << std::dec << ": " << "Unkown instruction" << std::endl;
+			else {
+				u32 instr = cpu.mem.read32(cpu.gprs[Regs::PC] + i * 4);
+				IR_ARM ir;
+				try {
+					Decoder::Decode(ir, instr);
+					std::cout << std::hex << cpu.gprs[Regs::PC] + i * 4 << " - " << instr << std::dec << ": " << std::hex << Disassembler::Disassemble(ir) << std::endl;
+				}
+				catch (...) {
+					std::cout << std::hex << cpu.gprs[Regs::PC] + i * 4 << " - " << instr << std::dec << ": " << "Unkown instruction" << std::endl;
+				}
 			}
 		}
 
