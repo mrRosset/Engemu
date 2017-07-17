8bb5346fa84c0e5d0fc0d9c3b0acb2bd1a40c08b
mrRosset <marius.rosset@epfl.ch>
2017-07-17 20:25:28 +0200

    add support for demangling function pointer and destructor with parameters

--- Scripts/Export_Converter.py
+++ Scripts/Export_Converter.py
@@ -10,8 +10,8 @@ class Buffer(deque):
     def get(self, how_many):
         return ''.join([self.popleft() for _ in range(how_many)])
 
-def get_num(first, buf):
-	num = first
+def get_num(buf):
+	num = ""
 	while len(buf) > 0 and buf.peek(1).isnumeric():
 		num+= buf.get(1)
 	return int(num)
@@ -22,8 +22,11 @@ def demangle_arg(buf):
 	isReference = False
 	isUnsigned = False
 	isConst = False
+	isFunction = False
 	hasTemplate = False
+	hasNamespace = False
 	arg = ""
+	template = ""
 
 	while arg == "":
 		letter = buf.get(1)
@@ -35,8 +38,15 @@ def demangle_arg(buf):
 			isUnsigned = True
 		elif letter == 'C':
 			isConst = True
+		elif letter == "F":
+			isFunction = True
+			arg = "(*)"
 		elif letter == 't':
 			hasTemplate = True
+		elif letter == 'Q' and buf.peek(1) == '2':
+			buf.get(1)
+			hasNamespace = True
+			arg = "::"
 		elif letter == 'i':
 			arg = "int"
 		elif letter == 'd':
@@ -45,19 +55,51 @@ def demangle_arg(buf):
 			arg = "void"
 		elif letter == 'c':
 			arg = "char"
+		elif letter == 's':
+			arg = "short"	
+		elif letter == 'l':
+			arg = "long"			
 		elif letter == 'f': #todo: check
 			arg = "float" 
+		elif letter == "G":
+			#doesn't seems to do much
+			letter = letter
 		elif letter.isnumeric():
-			num = get_num(letter, buf)
+			buf.appendleft(letter)
+			num = get_num(buf)
 			arg = buf.get(num)
+		else:
+			print("unknow letter for demangling", letter)
+			sys.exit(-1)
 
 	if hasTemplate:
 		#Todo: understand what they represent
-		if buf.get(2) != "1Z":
+		if buf.get(1) != "1":
+			print("unknow template format for demangling")
+			sys.exit(-1)
+		temp_l = buf.get(1)
+		if temp_l == 'Z':
+			template = demangle_arg(buf)
+		elif temp_l == 'i':
+			template = str(get_num(buf))
+		else:
 			print("unknow template format for demangling")
 			sys.exit(-1)
 
-	return ("unsigned " if isUnsigned else "") + arg + (("<" + demangle_arg(buf) + ">") if hasTemplate else "" ) + (" *" if isPointer else "") + (" const" if isConst else "") + (" &" if isReference else "") 
+	if hasNamespace:
+		arg = buf.get(get_num(buf)) + "::" + buf.get(get_num(buf))
+
+	if isFunction and '_' in buf.peek(len(buf)):
+		isPointer = False
+		function_args = []
+		m_function_args, rest = buf.peek(len(buf)).split('_')
+		m_function_args = Buffer(m_function_args)
+		buf.get(len(m_function_args) + 1)
+		while len(m_function_args) > 0:
+			function_args.append(demangle_arg(m_function_args))
+		arg = demangle_arg(buf) + ' (*)(' + ', '.join(function_args) + ')'
+
+	return ("unsigned " if isUnsigned else "") + arg + (("<" + template  + ">") if hasTemplate else "" ) + (" const" if isConst else "") + (" *" if isPointer else "") + (" &" if isReference else "") 
 
 def demangle_args(mangled_args):
 	buf = Buffer(mangled_args)
@@ -66,7 +108,7 @@ def demangle_args(mangled_args):
 		args.append(demangle_arg(buf))
 		if len(buf) > 1 and buf.peek(1) == 'T':
 			buf.get(1)
-			repetition_num = get_num(buf.get(1), buf)
+			repetition_num = get_num(buf)
 			repetition_num -= 1
 			while repetition_num > 0:
 				args.append(args[-1])
@@ -94,15 +136,23 @@ def demangle_name(mangled):
 		return mangled
 	elif mangled[0] == '.':
 		#destructors
+		if mangled[1] != '_':
+			print("unknow desctructor format")
+			sys.exit(-1)
 		const = ""
 		rest = mangled[2:len(mangled)]
-		if(rest[0] == "C"):
+		if rest[0] == "C":
 			rest = rest[1:len(rest)]
 			const = "const"
-		namespace, args = number_extract(rest)
-		return namespace + "::~" +  namespace + "(void)" + const
-		return mangled #todo undertand this case
-	elif mangled[0] == '_':
+		elif rest[0] == 't':
+			rest = rest[1:len(rest)]
+			namespace, args = number_extract(rest)
+			args = args[2:len(args)]
+			return namespace + "::~" +  namespace + demangle_args(args) + const
+		else:
+			namespace, args = number_extract(rest)
+			return namespace + "::~" +  namespace + "()" + const
+	elif mangled[0] == '_' and mangled[1].isnumeric():
 		#constructors
 		const = ""
 		rest = mangled[1:len(mangled)]
@@ -117,11 +167,15 @@ def demangle_name(mangled):
 		else:
 			const = ""
 			fname, rest = mangled.split("__")
-			if(rest[0] == "C"):
+			if rest[0] == "C":
 				rest = rest[1:len(rest)]
 				const = "const"
-			namespace, args = number_extract(rest)
-			return namespace + "::" +  fname + demangle_args(args) + const
+			if rest[0].isnumeric():
+				#has a namespace
+				namespace, args = number_extract(rest)
+				return namespace + "::" +  fname + demangle_args(args) + const
+			else:
+				return fname + demangle_args(rest) + const
 
 #key : ordinal
 #value : address
@@ -161,19 +215,31 @@ for ordinal in exports:
 		sys.exit(-1)
 	address = exports[ordinal]
 	name = idt[ordinal]
-	#print(address + ": " + name)
+	print(name)
+	print(address + ": " + demangle_name(name))
 
 
-print(demangle_name("PageSizeInBytes__7UserHalRi"))
-print(demangle_name("newL__5CBaseUi"))
-print(demangle_name("BinarySearchUnsigned__C17RPointerArrayBasePUiRi"))
-print(demangle_name("CalibrationPoints__7UserHalR21TDigitizerCalibration"))
-print(demangle_name("BuildVarArrayL__13CArrayPakBaseRPt13CArrayVarFlat1Zv"))
-print(demangle_name("Print__6RDebugGt11TRefByValue1ZC7TDesC16e"))
-print(demangle_name("AppendFormat__6TDes16Gt11TRefByValue1ZC7TDesC16P14TDes16Overflowe"))
-print(demangle_name("Pow10__4MathRdi"))
-print(demangle_name("Pow__4MathRdRCdT2"))
-print(demangle_name("_5RHeapRC6RChunkiiii"))
-print(demangle_name("_5RHeapi"))
-print(demangle_name("._5RHeap"))
-print(demangle_name("._10CCirBuffer"))
+#tests
+# print(demangle_name("PageSizeInBytes__7UserHalRi"))
+# print(demangle_name("newL__5CBaseUi"))
+# print(demangle_name("BinarySearchUnsigned__C17RPointerArrayBasePUiRi"))
+# print(demangle_name("CalibrationPoints__7UserHalR21TDigitizerCalibration"))
+# print(demangle_name("BuildVarArrayL__13CArrayPakBaseRPt13CArrayVarFlat1Zv"))
+# print(demangle_name("Print__6RDebugGt11TRefByValue1ZC7TDesC16e"))
+# print(demangle_name("AppendFormat__6TDes16Gt11TRefByValue1ZC7TDesC16P14TDes16Overflowe"))
+# print(demangle_name("Pow10__4MathRdi"))
+# print(demangle_name("Pow__4MathRdRCdT2"))
+# print(demangle_name("_5RHeapRC6RChunkiiii"))
+# print(demangle_name("_5RHeapi"))
+# print(demangle_name("._5RHeap"))
+# print(demangle_name("._10CCirBuffer"))
+# print(demangle_name("_DbgMarkEnd__4UserQ25RHeap12TDbgHeapTypei"))
+# print(demangle_name("PanicTFixedArray__Fv"))
+# print(demangle_name("DummyEuser_1659__Fv"))
+# print(demangle_name("Next__10TFindMutexRt4TBuf1i256"))
+# print(demangle_name("StringLength__4UserPCUs"))
+# print(demangle_name("SetExceptionHandler__7RThreadPF8TExcType_vUl"))
+# print(demangle_name("Find__C10RArrayBasePCvPFPCvPCv_i"))
+# print(demangle_name("IsPresent__C8TUidTypeG4TUid"))
+# print(demangle_name("._t13CArrayFixFlat1Zi"))
+# print(demangle_name("._t13CArrayFixFlat1Z4TUidi"))
