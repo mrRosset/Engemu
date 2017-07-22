4aa84e52b3a8cd6e58ea59f2742263ae19ca366f
mrRosset <marius.rosset@epfl.ch>
2017-07-22 19:03:46 +0200

    Remove commented old code

--- Engemu/Engemu.cpp
+++ Engemu/Engemu.cpp
@@ -65,48 +65,6 @@ int main(int argc, char* argv[])
 			std::this_thread::sleep_for(std::chrono::milliseconds(sleep_time));
 		}
 	}
-
-
-	/*while (true) {
-		for (int i = 0; i < 5; i++) {
-			if (cpu.cpsr.flag_T) {
-				u16 instr = cpu.mem.read16(cpu.gprs[Regs::PC] + i * 2);
-				IR_Thumb ir;
-				ir.instr = TInstructions::SWI;
-				try {
-					Decoder::Decode(ir, instr);
-					std::string text = Disassembler::Disassemble(ir);
-					std::cout << std::hex << cpu.gprs[Regs::PC] + i * 2 << " - " << instr << std::dec << ": " << std::hex << Disassembler::Disassemble(ir) << std::endl;
-				}
-				catch (...) {
-					std::cout << std::hex << cpu.gprs[Regs::PC] + i * 2 << " - " << instr << std::dec << ": " << "Unkown instruction" << std::endl;
-				}
-			}
-			else {
-				u32 instr = cpu.mem.read32(cpu.gprs[Regs::PC] + i * 4);
-				IR_ARM ir;
-				try {
-					Decoder::Decode(ir, instr);
-					std::cout << std::hex << cpu.gprs[Regs::PC] + i * 4 << " - " << instr << std::dec << ": " << std::hex << Disassembler::Disassemble(ir) << std::endl;
-				}
-				catch (...) {
-					std::cout << std::hex << cpu.gprs[Regs::PC] + i * 4 << " - " << instr << std::dec << ": " << "Unkown instruction" << std::endl;
-				}
-			}
-		}
-
-		std::cout << "\n\n";
-
-		for (int i = 0; i <= 15; i++) {
-			std::cout << Disassembler::Disassemble_Reg(i) << ": " << std::hex << cpu.gprs[i] << std::dec << std::endl;
-		}
-
-		std::cout << "N:" << cpu.cpsr.flag_N << " Z:" << cpu.cpsr.flag_Z << " C:" << cpu.cpsr.flag_C << " V:" << cpu.cpsr.flag_V << std::endl;
-
-		std::cin.get();
-		system("cls");
-		cpu.Step();
-	}*/
 	
 	return 0;
 }
