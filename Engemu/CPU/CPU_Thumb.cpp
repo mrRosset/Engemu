d3a317f3ff7492f5f863648f5c208487bd7c1ad7
mrRosset <marius.rosset@epfl.ch>
2017-08-20 17:23:30 +0200

    Consider BX R12 same as BX LR for call stack

--- Engemu/CPU/CPU_Thumb.cpp
+++ Engemu/CPU/CPU_Thumb.cpp
@@ -246,7 +246,7 @@ void CPU::Branch(IR_Thumb& ir) {
 	case TInstructions::BX:
 		cpsr.flag_T = !!(gprs[ir.operand1] & 0b1);
 		gprs[Regs::PC] = gprs[ir.operand1] & 0xFFFFFFFE;
-		if (ir.operand1 == Regs::LR && !call_stack.empty()) {
+		if ((ir.operand1 == Regs::LR || ir.operand1 == 12) && !call_stack.empty()) {
 			call_stack.pop_back();
 		}
 		else if (!call_stack.empty()) {
