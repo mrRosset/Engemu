4906ad261eb6919018e609f2ace0c218a6d0fd8a
mrRosset <marius.rosset@epfl.ch>
2017-09-02 20:39:25 +0200

    Add the possiblity to control amount of memory

--- Engemu/Tests/TestMemory.h
+++ Engemu/Tests/TestMemory.h
@@ -8,18 +8,18 @@ public:
 	std::vector<u8> ram;
 	u32 ram_cursor;
 
-	class TestMemory() : ram(0x1000) {
+	class TestMemory(u32 mem_size = 0x1000) : ram(mem_size) {
 		ram_cursor = 0;
 	}
 
 	inline u8 read8(u32 address) override {
 		if (address < ram.size()) return ram[address];
-		else throw std::string("Invalid read to unmapped memory");
+		else throw std::string("Invalid read to unmapped memory : ") + std::to_string(address);
 	}
 
 	inline void write8(u32 address, u8 value) override {
 		if (address < ram.size()) ram[address] = value;
-		else throw std::string("Invalid write to unmapped memory");
+		else throw std::string("Invalid write to unmapped memory: ") + std::to_string(address);
 	}
 
 	u32 allocateRam(u32 size) override {
